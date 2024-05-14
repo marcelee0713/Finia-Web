f7b3505d80188be12598490e6c6246b672ae059b
Marcel Magbual <magbualmarcel@gmail.com>
2024-05-14 21:31:55 +0800

    init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,56 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      animation: {
+        animfadeAbove: "fadeAbove 800ms ease-in-out",
+        animfadeBelow: "fadeBelow 800ms ease-in-out",
+        animfadeLeftSide: "fadeLeftSide 800ms ease-in-out",
+        animfadeRightSide: "fadeRightSide 800ms ease-in-out",
+        animFullHeight: "fullHeight 800ms ease-in-out",
+      },
+      keyframes: {
+        fadeAbove: {
+          "100%": { transform: "translateY(0px)", opacity: "1" },
+          "0%": {
+            transform: "translateY(-15px)",
+            opacity: "0",
+          },
+        },
+        fadeBelow: {
+          "100%": { transform: "translateY(0px)", opacity: "1" },
+          "0%": {
+            transform: "translateY(15px)",
+            opacity: "0",
+          },
+        },
+        fadeLeftSide: {
+          "100%": { transform: "translateX(0px)", opacity: "1" },
+          "0%": {
+            transform: "translateX(-15px)",
+            opacity: "0",
+          },
+        },
+        fadeRightSide: {
+          "100%": { transform: "translateX(0px)", opacity: "1" },
+          "0%": {
+            transform: "translateX(15px)",
+            opacity: "0",
+          },
+        },
+        fullHeight: {
+          "100%": { height: "100%" },
+          "0%": {
+            height: "0px",
+          },
+        },
+      },
+      fontFamily: {
+        openSans: ["Open Sans", "sans-serif"],
+      },
+      colors: {
+        primary: "#001220",
+        secondary: "#F7DC2A",
+        accent: "#DFDFDF",
       },
     },
   },
